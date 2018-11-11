fb4a4915e5152b5a1002eae405e1e40a1b3f6fdb
Otto G <ottodevs@gmail.com>
2018-11-11 18:13:04 +0100

    Create new tpt/ui package, and first component

--- apps/projects/app/index.js
+++ apps/projects/app/index.js
@@ -1,54 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import Aragon, { providers } from '@aragon/client'
-import App from './components/App/App'
+import { render } from 'react-dom'
+import { ConnectedApp } from '@tpt/extras-ui'
 
-// import { projectsMockData } from './utils/mockData'
+const root = document.getElementById('projects')
+render(<ConnectedApp />, root)
+// import { setConfig, cold } from 'react-hot-loader'
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const { whyDidYouUpdate } = require('why-did-you-update')
-//   whyDidYouUpdate(React)
+// setConfig({
+//   onComponentRegister: (type, name, file) =>
+//     file.indexOf('node_modules') > 0 && cold(type),
+// })
+
+// const renderApp = () => {
+//   const App = require('./components/App/ConnectedApp').default
+//   render(<App />, root)
 // }
 
-// TODO: Convert to stateless functional component
-class ConnectedApp extends React.Component {
-  state = {
-    app: new Aragon(new providers.WindowMessage(window.parent)),
-    observable: null,
-    userAccount: '',
-    // ...projectsMockData,
-  }
-  componentDidMount() {
-    window.addEventListener('message', this.handleWrapperMessage)
-  }
-  componentWillUnmount() {
-    window.removeEventListener('message', this.handleWrapperMessage)
-  }
-  // handshake between Aragon Core and the iframe,
-  // since iframes can lose messages that were sent before they were ready
-  handleWrapperMessage = ({ data }) => {
-    const {app} = this.state
-    if (data.from !== 'wrapper') {
-      return
-    }
-    if (data.name === 'ready') {
-      this.sendMessageToWrapper('ready', true)
-      this.setState({
-        observable: app.state(),
-      })
-      app.accounts().subscribe(accounts => {
-        this.setState({
-          userAccount: accounts[0] || '',
-        })
-      })
-    }
-  }
-  sendMessageToWrapper = (name, value) => {
-    window.parent.postMessage({ from: 'app', name, value }, '*')
-  }
-  render() {
-    return <App {...this.state} />
-  }
-}
-ReactDOM.render(<ConnectedApp />, document.getElementById('projects'))
-
+// renderApp()
+// module.hot.accept(renderApp)
