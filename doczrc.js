7406a3f2d23ba9f005d308dbf098becca4b9db1e
Otto G <ottodevs@gmail.com>
2018-12-05 00:21:50 +0100

    Check docz for component documentation

---
+++ doczrc.js
@@ -0,0 +1,20 @@
+// doczrc.js
+// import { myCoolPlugin } from 'docz-plugin-mycoolplugin'
+
+export default {
+  files: '**/*.{md,markdown,mdx}',
+  title: 'That Planning Suite',
+  description: 'Planning Suite Aragon App',
+  //   theme: 'my-coolest-theme',
+  //   themeConfig: {
+  //     colors: {
+  //       primary: 'tomato',
+  //     },
+  //   },
+  //   modifyBundlerConfig: config => {
+  //     /* do your magic here */
+  //   },
+  //   plugins: [
+  //     myCoolPlugin()
+  //   ]
+}
