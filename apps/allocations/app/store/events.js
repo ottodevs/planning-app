5265a6756e04f6cb7138948fa3d2ab6c0b703013
Otto G <ottodevs@gmail.com>
2019-02-20 15:55:24 +0100

    Fix allocations script edge case with state guard and fix import path

--- apps/allocations/app/store/events.js
+++ apps/allocations/app/store/events.js
@@ -3,6 +3,7 @@ import { onFundedAccount, onNewAccount, syncAccounts } from './account'
 import { onEntryAdded, onEntryRemoved } from './entry'
 
 export const handleEvent = async (state, { event, returnValues }) => {
+  if (!state) return
   const { addressBook, entries, accounts } = state
   let nextAccounts, nextEntries
 
