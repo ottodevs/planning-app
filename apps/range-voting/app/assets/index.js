f30867d0bf8663a97273bbd2a4aa4849ec633165
Otto G <ottodevs@gmail.com>
2019-02-20 13:01:32 +0100

    Range voting: assets cleanup

---
+++ apps/range-voting/app/assets/index.js
@@ -0,0 +1,9 @@
+export { default as arrowDownSvg } from './svg/arrow-down.svg'
+export { default as closeSvg } from './svg/close.svg'
+export { default as emptyCardSvg } from './svg/empty-card-icon.svg'
+export { default as newRangeVoteSvg } from './svg/new_range_vote.svg'
+export { default as transactionErrorSvg } from './svg/transaction-error.svg'
+export { default as transactionPendingSvg } from './svg/transaction-pending.svg'
+export { default as transactionSuccessSvg } from './svg/transaction-success.svg'
+
+export { default as IconRemove } from './components/Remove'
