e5ea2d2b9c2d02b85b8dbf037b9786a1f0077fdd
mat <mat@localhost.localdomain>
2019-05-24 02:39:08 +0200

    Fixed aragon entrypoint script

--- docker-system/aragon/docker-entrypoint.sh
+++ docker-system/aragon/docker-entrypoint.sh
@@ -17,57 +17,9 @@ start_testrpc() {
 
 echo "Starting our own testrpc instance at port $testrpc_port"
 start_testrpc
-sleep 5
-
-# Exit script as soon as a command fails.
-replace_manifest_path() {
-	file=$PWD/manifest.json
-	output=$PWD/dist/manifest.json
-	sed "s/dist\\///g" "$file" >"$output"
-}
-
-export -f replace_manifest_path
-
-copy_assets() {
-	mkdir -p dist/images && cp images/icon.svg dist/images/
-}
-
-export -f copy_assets
-
-deploy_contract() {
-	# TODO: Are we sure we want to mute truffle output? we should discuss this maybe
-	npx truffle compile >/dev/null
-	deployed_at=$(npx truffle migrate --reset | tail -4 | head -1 | awk '{ print $NF }')
-	echo "Deployed at:" "$deployed_at"
-	published=$(npm run publish:http -- --contract "$deployed_at" | tail -2)
-	echo "$published"
-	replace_manifest_path
-	copy_assets
-	sleep 2
-}
-
-export -f deploy_contract
-
-start_kit() {
-	# Exit error mode so the testrpc and parallel parcel instances always gets killed
-	set +e
-	result=0
-	npm run start:kit "$@"
-	echo "Terminated, wait the cleaning up..."
-	result=$?
-
-	echo "Terminating testrpc..."
-	kill -9 $testrpc_pid
-	echo "Terminating parcel instances..."
-	kill -9 $parallel_pid
-
-	exit $result
-}
-
-echo "Compiling and getting deployed contract address"
-npx lerna exec --scope=@tps/apps-* --concurrency=1 -- deploy_contract
-npx lerna exec --scope=@tps/apps-* -- aragon apm versions
-
+sleep 15
+npm run publish:http
 start_kit
+npm run start:kit
 
 exec "$@"
